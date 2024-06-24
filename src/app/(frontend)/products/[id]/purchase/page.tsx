2090bf68b592f532941e4998224be499d4a55c5a
tiwariParth <243mjparth@gmail.com>
2024-06-24 23:38:00 +0530

    chore: Update CheckoutForm component for product purchase page

--- src/app/(frontend)/products/[id]/purchase/page.tsx
+++ src/app/(frontend)/products/[id]/purchase/page.tsx
@@ -1,35 +1,33 @@
 import db from "@/db/db";
 import { notFound } from "next/navigation";
-import React from "react";
 import Stripe from "stripe";
-import CheckOutForm from "../_component/CheckOutForm";
+import { CheckoutForm } from "../_component/CheckOutForm";
 
-const stripe = new Stripe(String(process.env.STRIPE_SECRET_KEY));
-async function PurchasePage({ params: { id } }: { params: { id: string } }) {
-  const product = await db.product.findUnique({
-    where: { id },
-  });
 
-  if (!product) {
-    return notFound();
-  }
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export default async function PurchasePage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await db.product.findUnique({ where: { id } });
+  if (product == null) return notFound();
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: product.priceInCents,
-    currency: "INR",
+    currency: "USD",
     metadata: { productId: product.id },
   });
 
-  if (!paymentIntent) {
-    throw new Error("Failed to create payment intent");
+  if (paymentIntent.client_secret == null) {
+    throw Error("Stripe failed to create payment intent");
   }
 
   return (
-    <CheckOutForm
+    <CheckoutForm
       product={product}
       clientSecret={paymentIntent.client_secret}
     />
   );
 }
-
-export default PurchasePage;
