014eff0799d7b9d1c29d83a1491265b9db704b57
tiwariParth <243mjparth@gmail.com>
2024-06-27 14:39:42 +0530

    chore: Update page headers and navigation labels for consistency

--- src/app/(frontend)/sell/page.tsx
+++ src/app/(frontend)/sell/page.tsx
@@ -19,20 +19,19 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
-import { formatCurrency, formatNumber } from "@/lib/formartter";
-import { PageHeader } from "@/app/admin/_components/PageHeader";
 import {
   ActiveToggleDropdownItem,
   DeleteDropdownItem,
 } from "./_components/ProductActions";
+import { formatCurrency, formatNumber } from "@/lib/formartter";
+import { PageHeader } from "@/app/admin/_components/PageHeader";
 import { ProductForm } from "./_components/ProductForm";
 
-export default function Sell() {
+export default function AdminProductsPage() {
   return (
     <>
       <div className="flex justify-between items-center gap-4">
-        <PageHeader>Sell Your Products</PageHeader>
+        <PageHeader>Products</PageHeader>
       </div>
       <ProductsTable />
     </>
--- src/app/admin/userproducts/page.tsx
+++ src/app/admin/userproducts/page.tsx
@@ -1,9 +1,122 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
 
-const page = () => {
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import db from "@/db/db";
+import { CheckCircle2, MoreVertical, XCircle } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  ActiveToggleDropdownItem,
+  DeleteDropdownItem,
+} from "../../admin/products/_components/ProductActions";
+import { formatCurrency, formatNumber } from "@/lib/formartter";
+import { PageHeader } from "../_components/PageHeader";
+
+export default function AdminProductsPage() {
   return (
-    <div>HII from user poages</div>
-  )
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>User Products</PageHeader>
+      </div>
+      <ProductsTable />
+    </>
+  );
 }
 
-export default page
+async function ProductsTable() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      _count: { select: { orders: true } },
+    },
+    orderBy: { name: "asc" },
+  });
+
+  if (products.length === 0) return <p>No products found</p>;
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available For Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>
+              {product.isAvailableForPurchase ? (
+                <>
+                  <span className="sr-only">Available</span>
+                  <CheckCircle2 />
+                </>
+              ) : (
+                <>
+                  <span className="sr-only">Unavailable</span>
+                  <XCircle className="stroke-destructive" />
+                </>
+              )}
+            </TableCell>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+            <TableCell>{formatNumber(product._count.orders)}</TableCell>
+            <TableCell>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical />
+                  <span className="sr-only">Actions</span>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem asChild>
+                    <a download href={`/admin/products/${product.id}/download`}>
+                      Download
+                    </a>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href={`/admin/products/${product.id}/edit`}>
+                      Edit
+                    </Link>
+                  </DropdownMenuItem>
+                  <ActiveToggleDropdownItem
+                    id={product.id}
+                    isAvailableForPurchase={product.isAvailableForPurchase}
+                  />
+                  <DropdownMenuSeparator />
+                  <DeleteDropdownItem
+                    id={product.id}
+                    disabled={product._count.orders > 0}
+                  />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
